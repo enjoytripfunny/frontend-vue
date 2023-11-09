fd838926afbd3ded9f55f454fe60cd633dad5515
yuncheol-AHN <anchul@hanmail.net>
2023-11-09 23:52:39 +0900

    [ feat ] : login page form, no option

---
+++ src/components/Member/MemberLogin.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from "vue";
+const remember = ref();
+const memberLogin = () => {
+  // REST API를 통한 로그인 구현
+};
+</script>
+
+<!-- use bootstrap's card component -->
+<template>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="text-center">Login</h3>
+          </div>
+          <div class="card-body">
+            <form>
+              <div class="mb-3">
+                <label for="username" class="form-label">email</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="username"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div class="mb-3">
+                <label for="password" class="form-label">password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  placeholder="Enter your password"
+                />
+              </div>
+              <div class="mb-3 form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="rememberMe"
+                  v-model="remember"
+                />
+                <label class="form-check-label" for="rememberMe"
+                  >remember email</label
+                >
+              </div>
+              <button
+                type="submit"
+                class="btn btn-primary"
+                @click="memberLogin"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MemberLogin from "@/components/Member/MemberLogin.vue";
+</script>
+
+<template>
+  <MemberLogin></MemberLogin>
+</template>
+
+<style scoped></style>
