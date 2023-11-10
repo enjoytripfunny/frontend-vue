1e1dd5ce84bbcae4dce185f4822ff558a3c7289f
yuncheol-AHN <anchul@hanmail.net>
2023-11-10 09:19:46 +0900

    [ feat ] : signup form, no option

---
+++ src/views/SignupView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MemberSignup from "../components/Member/MemberSignup.vue";
+</script>
+
+<template>
+  <div>
+    <MemberSignup />
+  </div>
+</template>
+
+<style scoped></style>
