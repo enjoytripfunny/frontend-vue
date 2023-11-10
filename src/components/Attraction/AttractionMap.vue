8bd4d1b024f95a3fbd5cf5c6763c02e819e88245
yuncheol-AHN <anchul@hanmail.net>
2023-11-10 11:02:13 +0900

    [ style ] : AttractionMap : script -> script setup

--- src/components/Attraction/AttractionMap.vue
+++ src/components/Attraction/AttractionMap.vue
@@ -1,69 +1,33 @@
-<!-- <script setup>
-import { ref, onMounted } from 'vue'
-
-const initMap = () => {
-  console.log('start')
-  const container = document.getElementById('map')
-  const options = {
-    center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3
-  }
-  console.log('OK ?')
-  const map = new window.kakao.maps.Map(container, options)
-  map.setMapTypeId(window.kakao.maps.MapTypeId.HYBRID)
-}
+<script setup>
+import { ref, onMounted } from "vue";
+// import { axios } from "axios";
 
 onMounted(() => {
-  const script = document.createElement('script')
-  script.type = 'text/javascript'
+  // kakao api library를 가져오는 부분, kakao.maps를 통해 map method 접근
+  const script = document.createElement("script");
+
+  /* global kakao */
+  // script download 후 그리기 실행
+  script.onload = () =>
+    kakao.maps.load(() => {
+      const container = document.getElementById("map");
+      const options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+      console.log(kakao.maps);
+      console.log("OK ?");
+      const map = new kakao.maps.Map(container, options);
+      map.setMapTypeId(kakao.maps.MapTypeId.NORMAL);
+    });
 
   script.src =
-    '//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=066c4bf5fb8745fcc2b066ec145bb938'
-  script.async = true
+    "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=066c4bf5fb8745fcc2b066ec145bb938";
+  document.head.appendChild(script);
+});
+</script>
 
-  script.onload = () => {
-    // 스크립트가 로드된 후에 수행할 작업
-    console.log('Script loaded successfully!')
-    console.log(kakao.maps)
-    kakao.maps.load(initMap())
-  }
-
-  script.onerror = () => {
-    // 스크립트 로드 중 에러 발생 시 처리
-    console.error('Error loading script.')
-  }
-
-  document.head.appendChild(script)
-})
-
-// onMounted(async () => {
-//   if (window.kakao && window.kakao.maps) {
-//     console.log('.')
-//     initMap()
-//   } else {
-//     console.log('..')
-//     const script = document.createElement('script')
-//     /* global kakao */
-//     script.src =
-//       '//dapi.kakao.com/v2/maps/sdk.js?&autoload=false&appkey=066c4bf5fb8745fcc2b066ec145bb938'
-//     script.type = 'text/javascript'
-
-//     // 스크립트 로드를 Promise로 감싸서 로드 완료 후에 initMap 함수를 호출하도록 함
-//     await new Promise((resolve, reject) => {
-//       script.onload = resolve
-//       script.onerror = reject
-
-//       // html head tag에 script 추가 !!!
-//       document.template.appendChild(script)
-//     })
-//     console.log(script)
-//     // Kakao 지도 API가 로드된 후에 initMap 함수를 호출
-//     initMap()
-//   }
-// })
-</script> -->
-
-<script>
+<!-- <script>
 export default {
   mounted() {
     // kakao api library를 가져오는 부분 / kakao.maps를 통해 method 접근
@@ -88,7 +52,7 @@ export default {
     },
   },
 };
-</script>
+</script> -->
 
 <template>
   <div class="parent">
