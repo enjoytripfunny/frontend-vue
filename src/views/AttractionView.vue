186731d63cb0d0f2d33d53c34a6b38c8cd3ceb73
yuncheol-AHN <anchul@hanmail.net>
2023-11-13 23:07:07 +0900

    [ feat ] : attraction

--- src/views/AttractionView.vue
+++ src/views/AttractionView.vue
@@ -1,10 +1,8 @@
 <script setup>
-import SelectLocation from "@/components/Attraction/SelectLocation.vue";
 import AttractionMap from "../components/Attraction/AttractionMap.vue";
 </script>
 
 <template>
-  <SelectLocation></SelectLocation>
   <AttractionMap></AttractionMap>
 </template>
 
