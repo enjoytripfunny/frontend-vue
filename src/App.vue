31f1c934d89f07a16d30672f05af96de7399d16b
yuncheol-AHN <anchul@hanmail.net>
2023-11-13 15:51:02 +0900

    [ style ] : router view

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 import HeadingNavigationBar from "./components/common/HeadingNavigationBar.vue";
 
-// import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <div>
     <heading-navigation-bar />
-    <h1>Enjoy Trip</h1>
+
+    <!-- Map -->
     <router-view></router-view>
+
+    <!-- footer -->
   </div>
 </template>
 
